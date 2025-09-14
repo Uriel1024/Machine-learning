b6e2ac721a446865f31128c0ad092a593ad32d56
Uriel Velazquez <evelazqueza1900@alumno.ipn.mx>
2025-09-14 10:51:52 -0600

    .

--- practica_2/herencia.py
+++ practica_2/herencia.py
@@ -1,11 +1,12 @@
 import random
+import math
 
 cromosomas = 20
 parejas = 50
 
 def gen_padres():
-	madre = [[0 for i in range(cromosomas)] for j in range (parejas)] 
-	padre = [[0 for i in range(cromosomas)] for j in range(parejas)] 
+	madre = [[0 for _ in range(cromosomas)] for _ in range (parejas)] 
+	padre = [[0 for _ in range(cromosomas)] for _ in range(parejas)] 
 	for i in range(parejas):
 		for j in range (cromosomas):
 			madre[i][j] = random.randint(1,9)
@@ -14,17 +15,18 @@ def gen_padres():
 
 
 def herencia(padres, madres):	
-	hijo1 = [[0 for i in range(cromosomas)] for j in range (parejas)]
-	hijo2 = [[0 for i in range(cromosomas)] for j in range (parejas)]
+	hijo1 = [[0 for _ in range(cromosomas)] for _ in range (parejas)]
+	hijo2 = [[0 for _ in range(cromosomas)] for _ in range (parejas)]
 
 	r1 , r2 = incesto()
+
 
 	for i in range(parejas):
 		for j in range(cromosomas):
 			suma = padres[r1[i]][j] + madres[r2[i]][j]
 			if  suma %2 == 0:
 				hijo1[i][j] = suma //2
-				hijo1[i][j] = suma //2
+				hijo2[i][j] = suma //2
 			else:
 				if random.random()  < 0.5:
 					hijo1[i][j] = suma //2
@@ -33,6 +35,9 @@ def herencia(padres, madres):
 					hijo2[i][j] = suma //2
 					hijo1[i][j] = (suma //2) + 1
 	return hijo1,hijo2
+
+
+def torneo():
 
 
 
@@ -56,15 +61,28 @@ def incesto():
 
 def mutacion(hijos):
 	for i in range(parejas):
-		if random.random() < 0.001: #el minimo, en caso de ser necesario se debe de agregar un poco mas (hasta 25%)
+		if random.random() < 0.15: #el minimo, en caso de ser necesario se debe de agregar un poco mas (hasta 25%)
 			j = random.randint(0, cromosomas -1 )
 			hijos[i][j] = random.randint(1,9)
 	return hijos
 
+def incesto():
+	hermanos = True
+	hermano1 = list(range(parejas))
+	hermano2 = list(range(parejas))
+	while hermanos:
+		hermanos = False
+		random.shuffle(hermano1)
+		random.shuffle(hermano2)
+		for i in range(parejas):
+			if hermano1[i] == hermano2[i]:
+				hermanos = True
+	return hermano1, hermano2
+
 
 #para pode encontrar el hijo perfecto (todos los cromosomas con valor de 9). 
 def ario(personas):
-    return any(all(c == 0 for c in individuo) for individuo in personas)
+    return any(all(c == 9 for c in individuo) for individuo in personas)
 
 if __name__ == "__main__":
     padres, madres = gen_padres()
@@ -80,12 +98,15 @@ if __name__ == "__main__":
         print(f"Generación: {generaciones}\n")
 
         hijo1, hijo2 = herencia(hijo1, hijo2)
+        print(f"hijo1 antes de mutacion:{hijo1} ")
+        print(f"hijo2 antes de mutacion:{hijo2} ")
+
         hijo1 = mutacion(hijo1)
         hijo2 = mutacion(hijo2)
         if ario(hijo1) or ario(hijo2):
             hijo_perfecto = True
-        print(hijo1)
-        print(hijo2)
+        print(f"hijo 1: {hijo1}")
+        print(f"hijo 2: {hijo2}")
 
     print(f"\nÚltima generación:\nHijo1: {hijo1}\nHijo2: {hijo2}")
     print(f"\nDespués de {generaciones} generaciones se encontró al hijo perfecto.")
--- practica_2/pruebas.py
+++ practica_2/pruebas.py
@@ -12,25 +12,61 @@ def gen_padres():
 			padre[i][j] = random.randint(1,9)
 	return padre, madre
 
-def herencia(padre, madre):
-    # Cruza los cromosomas de los padres para crear dos hijos con promedios
-    hijo_1 = [
-        [(padre[i][j] + madre[i][j]) // 2 for j in range(cromosomas)]
-        for i in range(parejas)
-    ]
-    hijo_2 = [
-        [((padre[i][j] + madre[i][j]) + 1) // 2 for j in range(cromosomas)]
-        for i in range(parejas)
-    ]
-    return hijo_1, hijo_2
+
+def incesto():
+    hermanos = True
+    hermano1 = list(range(parejas))
+    hermano2 = list(range(parejas))
+    while hermanos:
+        hermanos = False
+        random.shuffle(hermano1)
+        random.shuffle(hermano2)
+        for i in range(parejas):
+            if hermano1[i] == hermano2[i]:
+                hermanos = True
+    return hermano1, hermano2
+
+
+def herencia(padres, madres):   
+    hijo1 = [[0 for _ in range(cromosomas)] for _ in range (parejas)]
+    hijo2 = [[0 for _ in range(cromosomas)] for _ in range (parejas)]
+
+    r1 , r2 = incesto()
+
+    for i in range(parejas):
+        for j in range(cromosomas):
+            suma = padres[r1[i]][j] + madres[r2[i]][j]
+            #hijo1[i][j] = suma // 2
+            #hijo2[i][j] = (suma + 1) // 2
+            if suma % 2 == 0:
+                hijo1[i][j] = suma //2
+                hijo2[i][j] = suma //2
+            else:
+                if random.random() < .5:
+                    hijo1[i][j] = suma //2
+                    hijo2[i][j] = (suma //2) + 1 
+                else:
+                    hijo2[i][j] = suma //2
+                    hijo1[i][j] = (suma //2) + 1 
+    return hijo1,hijo2
 
 
 
-padre, madre = gen_padres()
-hijo1 , hijo2 = herencia(padre,madre)
+if __name__ == "__main__":
+    padres, madres = gen_padres()
+    print(f"Los padres son {padres}")
+    print(f"Las madres son {madres}")
 
-print(f"Padre 1: {padre}\n\n\n")
-print(f"madre 1: {madre}\n\n\n")
+    hijo1, hijo2 = herencia(padres, madres)
+    generaciones = 0
+    hijo_perfecto = False
 
-print(f"Hijo 1: {hijo1}\n\n\n")
-print(f"Hijo 2: {hijo2}")
+
+    while generaciones != 4:
+        generaciones += 1
+        print(f"\n\nGeneración: {generaciones}\n")
+        
+        hijo1, hijo2 = herencia(hijo1, hijo2)
+        print(f"hijo1 antes de mutacion:{hijo1} ")
+        print(f"hijo2 antes de mutacion:{hijo2} ")
+
